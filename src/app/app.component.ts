084bf24c8e6c1f89089294a9a007b4d5964f9a36
Ethan Russell <Ethan.Russell@mclaneco.com>
2020-07-05 16:44:54 -0500

    Adding minimus weather app functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,57 @@
-import {Component, OnInit} from '@angular/core';
 import {SlackService} from './services/slack.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UiService} from './services/ui/ui.service';
+import {Router} from '@angular/router';
+import {FbService} from './services/fb/fb.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'dia-alpha';
+  showMenu = false;
+  darkModeActive: boolean;
 
+  userEmail = '';
+  loggedIn = this.fb.isAuth();
+  sub1;
 
-  constructor(private slackService: SlackService) {
+  constructor(public ui: UiService, public fb: FbService, public router: Router, private slackService: SlackService) {
+
   }
 
-
   ngOnInit(): void {
+    this.sub1 = this.ui.darkModeState.subscribe((value) => {
+      this.darkModeActive = value;
+    });
+
+    this.fb.auth.userData().subscribe((user) => {
+      this.userEmail = user.email;
+    });
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  modeToggleSwitch() {
+    this.ui.darkModeState.next(!this.darkModeActive);
+  }
+
+  ngOnDestroy() {
+    this.sub1.unsubscribe();
+  }
+
+  logout() {
+    this.toggleMenu();
+    this.router.navigateByUrl('/login');
+    this.fb.auth.signout();
   }
 
   throwError() {
     this.slackService.postErrorOnSlack(new Error('Infinity Error'));
   }
 }
-
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,45 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+
+import {HomeComponent} from './pages/home/home.component';
+import {DetailsComponent} from './pages/details/details.component';
 import {HttpClientModule} from '@angular/common/http';
+import {WeatherCardComponent} from './ui/weather-card/weather-card.component';
+import {AddCardComponent} from './ui/add-card/add-card.component';
+import {AddComponent} from './pages/add/add.component';
+import {LoginComponent} from './pages/login/login.component';
+import {SignupComponent} from './pages/signup/signup.component';
+import {ErrorComponent} from './ui/error/error.component';
+import {AngularFireLite} from 'angularfire-lite';
+import {environment} from '../environments/environment';
+import {NguiAutoCompleteModule} from '@ngui/auto-complete';
+import {FormsModule} from '@angular/forms';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent,
+    WeatherCardComponent,
+    AddCardComponent,
+    AddComponent,
+    LoginComponent,
+    SignupComponent,
+    ErrorComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
+    AppRoutingModule,
     HttpClientModule,
-    AppRoutingModule
+    NguiAutoCompleteModule,
+    FormsModule,
+    AngularFireLite.forRoot(environment.config),
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+    BrowserAnimationsModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
